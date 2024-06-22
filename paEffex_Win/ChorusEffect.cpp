90d8480830de818dadeb4a2df82d5e0a0fcc746b
OyvinidIO <oyvind.uh@gmail.com>
2024-06-22 16:41:45 +0200

    Remove goto functionality.

--- paEffex_Win/ChorusEffect.cpp
+++ paEffex_Win/ChorusEffect.cpp
@@ -20,13 +20,13 @@ int ChorusEffect::applyEffect(float* iData, float* oData, unsigned long bufferSi
 	unsigned long channels = 2;
 	for (unsigned long i = 0; i < channels * bufferSize; i += channels)
 	{
-		oscillator = std::sinf(2.0f * 3.14159f * frequency* oscillatortime)/3.0;
+		oscillator = std::sinf(2.0f * 3.14159f * frequency * oscillatortime) / 3.0;
 		oscillatortime += 1.0f / 44100.0f;
 		if (oscillatortime >= 44100.0f)
 			oscillatortime = 0;
 
-		oData[i] = iData[i] + iData[i]* oscillator;
-		oData[i + 1] = iData[i + 1] + iData[i + 1]*oscillator;
+		oData[i] = iData[i] + iData[i] * oscillator;
+		oData[i + 1] = iData[i + 1] + iData[i + 1] * oscillator;
 	}
 
 	return 0;
--- paEffex_Win/main.cpp
+++ paEffex_Win/main.cpp
@@ -18,61 +18,78 @@ EffectManager effectManager;
 #define SAMPLE_RATE (44100)
 #define FRAMES_PER_BUFFER (64)
 
-int processAudioStreamCallback(const void *input, void *output, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo*, PaStreamCallbackFlags, void * userData) {
+int processAudioStreamCallback(const void* input, void* output, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo*, PaStreamCallbackFlags, void* userData) {
 
 	effectManager.applyEffect(input, output, framesPerBuffer);
 	return 0;
 }
+
+void ExitProgram(PaError err, string errorMsg) {
+	
+	
+	cout << "Exiting program" << endl;
+	
+	if(!errorMsg.empty())
+		cout << "Error: " << errorMsg << endl;
+
+	if (err != paNoError) {
+
+		fprintf(stderr, "Error number: %d\n", err);
+		fprintf(stderr, "Error message: %s\n", Pa_GetErrorText(err));
+	}
+
+	Pa_Terminate();
+	exit(0);
+}
+
 int main(void);
 int main(void) {
-	
+
 	/* Stream parameters */
-	PaStream *stream;
+	PaStream* stream;
 	PaError err = paNoError;
 	PaStreamParameters inputParameters, outputParameters;
 	PaStreamFlags streamFlags = paNoFlag; /*paClipOff paDitherOff paNeverDropInput paPrimeOutputBuffersUsingStreamCallback paPlatformSpecificFlags*/
 	PaHostApiInfo APIInfo;
-	
+
 
 	/* PA info */
 	const PaDeviceInfo* inputInfo;
 	const PaDeviceInfo* outputInfo;
 	PaHostApiIndex ApiIndex;
-	const PaHostApiInfo * HostApiInfo;
-	
+	const PaHostApiInfo* HostApiInfo;
+
 	/* Internal variables for main()*/
 	string waitForUserInput;
-	
+
 
 	/* Initialize PortAudio */
 	err = Pa_Initialize();
 	if (err != paNoError) {
-		cout << "PA Initialize FAILED" << endl;
-		return 0;
-	}			
-	
-	/* Initialize Stream Parameters */	
-	inputParameters.device = Pa_GetDefaultInputDevice();
-	outputParameters.device = Pa_GetDefaultOutputDevice();
+		ExitProgram(err, "PortAudio failed to initialize");
+	}
+
+	/* Initialize Stream Parameters */
+	auto deviceCount = Pa_GetDeviceCount();
+	inputParameters.device = 1; // Pa_GetDefaultInputDevice();	
+	outputParameters.device = 1; //	Pa_GetDefaultOutputDevice();
 
 	if (inputParameters.device == paNoDevice) {
-		cout << "Error: No default input device." << endl;
-		goto error;		
+		ExitProgram(err, "No default input device");
 	}
 	if (outputParameters.device == paNoDevice) {
-		cout << "Error: No default output device." << endl;
-		goto error;
-	}	
+		ExitProgram(err, "No default output device");
+	}
 	inputInfo = Pa_GetDeviceInfo(inputParameters.device);
 	outputInfo = Pa_GetDeviceInfo(outputParameters.device);
 
-	inputParameters.channelCount = 2;//  inputInfo->maxInputChannels;
-	outputParameters.channelCount = 2;//outputInfo->maxOutputChannels;
+	inputParameters.channelCount = inputInfo->maxInputChannels;
+	outputParameters.channelCount = outputInfo->maxOutputChannels;
 	inputParameters.sampleFormat = paFloat32; /*+paNonInterleaved;*/
 	outputParameters.sampleFormat = paFloat32; /*+paNonInterleaved;*/
 	inputParameters.suggestedLatency = inputInfo->defaultLowInputLatency;
 	outputParameters.suggestedLatency = outputInfo->defaultLowOutputLatency;
-	inputParameters.hostApiSpecificStreamInfo  = NULL;
+	inputParameters.hostApiSpecificStreamInfo = NULL;
 	outputParameters.hostApiSpecificStreamInfo = NULL;
 
 	/*-----------------------------------------------------------------------------*/
@@ -104,83 +121,68 @@ int main(void) {
 	cout << "      Type:              " << HostApiInfo->type << endl;*/
 
 
-	
+
 	// Example show ASIO panel
 	//PaAsio_ShowControlPanel(inputParameters.device, nullptr);
 	/*----------------------------------------------------------------------------------*/
-	
+
 	/* Create Effects */
 	effectManager.setEffect(new DelayEffect());
 	effectManager.setEffect(new ChorusEffect());
 
 	/* Open an audio I/O stream. */
 	cout << " -- Opening stream -- " << endl;
-	err = Pa_OpenStream(&stream,									
-						&inputParameters, &outputParameters, 
-						SAMPLE_RATE, 
-						FRAMES_PER_BUFFER,
-						streamFlags, 
-						processAudioStreamCallback, 
-						NULL);
-	if (err != paNoError) { 
-		cout << "--Error : Open stream failed. Errorcode: " << err << " : " << Pa_GetErrorText(err) << endl; 
-		goto error;
+	err = Pa_OpenStream(&stream,
+		&inputParameters, &outputParameters,
+		SAMPLE_RATE,
+		FRAMES_PER_BUFFER,
+		streamFlags,
+		processAudioStreamCallback,
+		NULL);
+	if (err != paNoError) {
+		ExitProgram(err, "Failed to open stream");
 	}
 	cout << " -- Opening stream successful -- " << endl;
 
 	/* Start an audio I/O stream. */
 	err = Pa_StartStream(stream);
 	if (err != paNoError) {
-		cout << "--Error : Start stream failed. Errorcode: " << err << " : " << Pa_GetErrorText(err) << endl;
-		goto error;
+		ExitProgram(err, "Failed to start stream");
 	}
 	cout << " -- Starting stream successful -- " << endl;
 
 	/*  -- Block thread until user hits enter -- */
-	cout << endl << "... play ... " << endl;	
-	while(true)
+	cout << endl << "... play ... " << endl;
+	while (true)
 	{
 		cin >> waitForUserInput;
-		break;		
+		break;
 	}
 
 	/* Stop an audio I/O stream. */
 	err = Pa_StopStream(stream);
 	if (err != paNoError) {
-		cout << "--Error : Stop stream failed. Errorcode: " << err << " : " << Pa_GetErrorText(err) << endl;
-		goto error;
+		ExitProgram(err, "Failed to stop stream");
 	}
 
 	/* Close an audio I/O stream. */
 	err = Pa_CloseStream(stream);
 	if (err != paNoError) {
-		cout << "--Error : Close stream failed. Errorcode: " << err << " : " << Pa_GetErrorText(err) << endl;
-		goto error;
+		ExitProgram(err, "Failed to close stream");
 	}
 
 	/* Terminate PortAudio */
 	err = Pa_Terminate();
 	if (err != paNoError) {
-		cout << "--Error : Terminate stream failed. Errorcode: " << err << " : " << Pa_GetErrorText(err) << endl;
-		goto error;
+		ExitProgram(err, "Failed to terminate stream");
 	}
-	cout << "Stream terminated" << endl;
-	
-
-error:
-	Pa_Terminate();
-	fprintf(stderr, "An error occured while using the portaudio stream\n");
-	fprintf(stderr, "Error number: %d\n", err);
-	fprintf(stderr, "Error message: %s\n", Pa_GetErrorText(err));
 
 	// Halt the program to read messages written to screen if an error occurred
-	/*while (true)
-	{
-		cin >> waitForUserInput;
-		break;
-	}*/
+	//while (true)
+	//{
+	//	cin >> waitForUserInput;
+	//	break;
+	//}
 
-
-	return err;
-	
+	return 0;
 }
